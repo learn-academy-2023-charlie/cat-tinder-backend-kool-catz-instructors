6cd829aba8d129bff7bda9ea33014bee45314528
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2023-06-05 14:36:18 -0700

    API endpoints

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,27 @@
 class CatsController < ApplicationController
+
+  def index
+    cats = Cat.all
+    render json: cats
+  end
+
+  def create
+    cat = Cat.create(cat_params)
+    if cat.valid?
+      render json: cat
+    else
+      render json: cat.errors, status: 422
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :age, :hobby, :image)
+  end
 end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'gets a list of cats' do
+      Cat.create(
+        name:'Garfield', 
+        age:6, 
+        hobby:'eating lasagna', 
+        image:'https://freesvg.org/img/OnlyWine-186.png'
+      )
+      get '/cats'
+      cat = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
   end
+
+  describe "POST /create" do
+    it 'creates a cat' do
+      cat_params = {
+        cat: {
+          name:'Garfield', 
+          age:6, 
+          hobby:'eating lasagna', 
+          image:'https://freesvg.org/img/OnlyWine-186.png'
+        }
+      }
+      post '/cats', params: cat_params
+      expect(response).to have_http_status(200)
+      cat = Cat.first
+      expect(cat.name).to eq 'Garfield'
+      expect(cat.age).to eq 6
+      expect(cat.hobby).to eq 'eating lasagna'
+    end
+  end
+
 end
